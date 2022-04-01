419cf8ff1ea56c9f3810c6e3825202a1912bf47c
Hussain Yousuf <hussainyousuf@folio3.com>
2022-04-01 23:52:53 +0500

    rod cutting with sol

--- rodcut.py
+++ rodcut.py
@@ -3,30 +3,33 @@ from functools import cache
 
 
 def rodcut(arr, n):
-    holder = {}
 
     @cache
-    def _rodcut(n):
+    def _rodcut(n, is_base_call=False):
         if(n == 0):
-            return [0, 0]
+            return 0
         max_sum = 0
-        ith_price = 0
+        ith_cut = 0
         for i in range(1, min(n+1, len(arr)+1)):
-            rest_sum, rest_ith_price = _rodcut(n-i)
-            sum = arr[i-1] + rest_sum
+            sum = arr[i-1] + _rodcut(n-i)
             if(sum > max_sum):
                 max_sum = sum
-                ith_price = arr[i-1]
-                holder[rest_sum] = rest_ith_price
+                ith_cut = i
 
-        return [max_sum, ith_price]
+        return [max_sum, ith_cut] if is_base_call else max_sum
 
-    x = _rodcut(n)
-    print(holder)
-    return x
+    values = []
+    max_sum, ith_cut = _rodcut(n, True)
+    values.append(ith_cut)
+    n = n - ith_cut
+    while(n != 0):
+        _, ith_cut = _rodcut(n, True)
+        values.append(ith_cut)
+        n = n - ith_cut
+    return (max_sum, values)
 
 
-print(rodcut([1, 5, 8, 9, 10, 17, 17, 20, 24, 30], 30))
+print(rodcut([1, 5, 8, 9, 10, 17, 17, 20, 24, 30], 2))
 
 
 def subset_sum_repeating(arr, target, sum=0, acc=[]):
