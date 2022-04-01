20a0992d80877647ea1e8af07ebcfd1f53c905a6
Hussain Yousuf <hussainyousuf@folio3.com>
2022-04-01 18:15:15 +0500

    ...

--- rodcut.py
+++ rodcut.py
@@ -1,19 +1,32 @@
 import math
+from functools import cache
 
 
 def rodcut(arr, n):
-    if(n == 0):
-        return 0
-    maxPrice = -math.inf
-    for i in range(1, n+1):
-        maxPrice = max(arr[i-1] + rodcut(arr, n-i), maxPrice)
-    return maxPrice
+    holder = {}
 
-    # [0......................10]
-    # [1 5 8 9 10 17 17 20 24 30]
+    @cache
+    def _rodcut(n):
+        if(n == 0):
+            return [0, 0]
+        max_sum = 0
+        ith_price = 0
+        for i in range(1, min(n+1, len(arr)+1)):
+            rest_sum, rest_ith_price = _rodcut(n-i)
+            sum = arr[i-1] + rest_sum
+            if(sum > max_sum):
+                max_sum = sum
+                ith_price = arr[i-1]
+                holder[rest_sum] = rest_ith_price
+
+        return [max_sum, ith_price]
+
+    x = _rodcut(n)
+    print(holder)
+    return x
 
 
-print(rodcut([1, 5, 8, 9, 10, 17, 17, 20, 24, 30], 5))
+print(rodcut([1, 5, 8, 9, 10, 17, 17, 20, 24, 30], 30))
 
 
 def subset_sum_repeating(arr, target, sum=0, acc=[]):
@@ -33,4 +46,4 @@ def subset_sum_repeating(arr, target, sum=0, acc=[]):
     return subset_sum_repeating(new_arr, target, sum, new_acc)
 
 
-print(subset_sum_repeating([3, 4], 7))
+# print(subset_sum_repeating([3, 4], 7))
