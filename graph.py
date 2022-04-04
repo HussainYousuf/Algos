a1947d19d66313f523b4f2b14d0335214211aafa
Hussain Yousuf <hussainyousuf@folio3.com>
2022-04-05 01:03:14 +0500

    dfs and dags

--- graph.py
+++ graph.py
@@ -1,51 +1,82 @@
-# domain (u,v,e)
-def from_edges(edges):
-    graph = {}
-    for u, v, *w in edges:
-        w = w[0] if w else None
-
-        if(v and v not in graph):
-            graph[v] = []
-
-        children = []
-        if(u in graph):
-            children = graph[u]
-        else:
-            graph[u] = children
-        children.append(v)
-
-    return graph
+from collections import defaultdict
 
 
-def mark_all_unvisited(graph):
-    for visited, _ in graph.values():
-        visited[0] = False
+class Graph:
+
+    def __init__(self) -> None:
+        self.indegree = defaultdict(int)
+        self.weight = defaultdict(dict)
+        self.visited = defaultdict(bool)
+        self.pre_num = defaultdict(int)
+        self.post_num = defaultdict(int)
+        self.counter = int()
+        self.components = []
+
+    def from_edges(self, edges):
+        for u, v, *w in edges:
+            if(u):
+                self.indegree[u]
+            if(v):
+                self.weight[u][v] = w[0] if w else None
+                self.indegree[v] += 1
+
+    def get_counter(self):
+        self.counter += 1
+        return self.counter
+
+    def dfs(self, u):
+        weight = self.weight
+        visited = self.visited
+        pre_num = self.pre_num
+        post_num = self.post_num
+
+        visited[u] = True
+        pre_num[u] = self.get_counter()
+        for v in weight[u].keys():
+            if(not visited[v]):
+                self.dfs(v)
+        post_num[u] = self.get_counter()
+
+    def mark_all_unvisited(self):
+        self.visited = defaultdict(bool)
+
+    def reset_counter(self):
+        self.counter = int()
+
+    def dfs_all(self):
+        visited = self.visited
+        indegree = self.indegree
+        for u in sorted(indegree, key=indegree.get):
+            if(visited[u]):
+                continue
+            self.dfs(u)
+            self.reset_counter()
+            self.components.append(dict(self.pre_num))
+            self.pre_num = defaultdict(int)
+        self.mark_all_unvisited()
+
+    def all_DAGs(self):
+        weight = self.weight
+        for component in self.components:
+            for u, pre_num in component.items():
+                cycle = False
+                for v in weight[u].keys():
+                    if(pre_num > component[v]):
+                        cycle = True
+                        print(f"break because of cycle from edge {u} to {v}")
+                        break
+                if(cycle):
+                    break
+                print(u)
+            print("----------")
 
 
-counter = 1
-
-
-def dfs(graph, v):
-    visited, children = graph[v]
-    if(not visited[0]):
-        visited[0] = True
-        print(v)
-
-        for child, _ in children:
-            dfs(graph, child)
-
-
-def dfs_all(graph):
-    print(graph)
-    for vertex, (visited, _) in graph.items():
-        if(not visited[0]):
-            dfs(graph, vertex)
-        mark_all_unvisited(graph)
-        global counter
-        counter = 1
-        print("-------")
-
-
-print(dfs_all(from_edges([
-    ('A', 'B'), ('A', 'E'), ('E', 'I'), ('I', 'J'), ('J', 'E')
-])))
+g = Graph()
+g.from_edges([
+    ('J', 'E'), ('A', 'E'), ('A', 'B'),
+    ('E', 'I'), ('I', 'J'), ('F', None),
+    ('C', 'D'),
+])
+g.dfs_all()
+g.all_DAGs()
+print(g.components)
