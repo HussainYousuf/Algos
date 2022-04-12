a3a4ad2157b2c24cd9427c79d41164eef488b4f2
Hussain Yousuf <hussainyousuf@folio3.com>
2022-04-13 03:55:19 +0500

    ...

--- merge_sort.py
+++ merge_sort.py
@@ -2,6 +2,19 @@
 
 import math
 
+# recursive def of merge but is not fast
+# def merge(left, right, arr):
+#     if(not left):
+#         arr += right[:]
+#     elif(not right):
+#         arr += left[:]
+#     elif(left[0] < right[0]):
+#         arr.append(left[0])
+#         merge(left[1:], right, arr)
+#     else:
+#         arr.append(right[0])
+#         merge(left, right[1:], arr)
+
 
 def merge_sort(arr):
     if(len(arr) < 2):
@@ -9,8 +22,8 @@ def merge_sort(arr):
     mid = (0 + len(arr)) // 2
     left = arr[:mid]
     right = arr[mid:]
-    merge_sort(left) # sort left inplace
-    merge_sort(right) # sort right inplace
+    merge_sort(left)  # sort left inplace
+    merge_sort(right)  # sort right inplace
 
     left.append(math.inf)
     right.append(math.inf)
